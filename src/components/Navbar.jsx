84d1bd4d880f9ade5f00e3edbe33fd43e210ebf1
Rashedul Hassan <rashedul@outlook.com>
2023-10-11 22:38:45 +0100

    Small changes in the firt half of the section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
+import { menu, close, javascript } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -62,10 +62,10 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={javascript} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Liron &nbsp;
-            <span className="sm:block hidden"> | 3D Portfolio</span>
+            Essex Studios &nbsp;
+            <span className="sm:block hidden"> | &nbsp;Portfolio</span>
           </p>
         </Link>
 
